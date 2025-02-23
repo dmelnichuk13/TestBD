eb25bb2caf9e7a415265287c77766240fecbd2a7
Dmitriy Melnichuk <dmelnichuk90@gmail.com>
2025-02-23 21:41:16 +0300

    jdbcTest

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection.createTable();
+        DatabaseConnection.clearTable();
+        DatabaseConnection.addEmployee("First", "Last", "Middle", "Position");
+        DatabaseConnection.getAllEmployees();
+        DatabaseConnection.addEmployee("First 1", " Last 1", "Middle 1", "Position 1");
+        DatabaseConnection.getAllEmployees();
+        DatabaseConnection.deleteEmployee(2);
+        DatabaseConnection.getAllEmployees();
     }
 }
